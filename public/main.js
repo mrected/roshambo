b360a17e22e8a7a95d5fa3bdd335fecd67d92932
Chris Terrazas <chris.terrazas@gmail.com>
2018-10-31 13:13:04 -0400

    logic works

--- public/main.js
+++ public/main.js
@@ -1,35 +1,50 @@
 const main = () => {
 
-  // const deciderArray = [
-  //   ['t', 'l', 'w'],
-  //   ['w', 't', 'l'],
-  //   ['l', 'w', 't']
-  // ]
-
-  // if player 1 and player 2 choices are the same - tie
-  // if player 1's choice  
-
-
   const playerOneButtons = document.querySelectorAll('.player-one button')
   const playerTwoButtons = document.querySelectorAll('.player-two button')
-  let playerOneChoice = ''
-  let playerTwoChoice = ''
+  let playerChoices = ['', '']
+  let havePlayersChosen = [false, false]
 
   const decision = () => {
-    if ()
+    if (playerChoices[0] === playerChoices[1]) {
+      console.log('it\'s a tie')
+    } else {
+      console.log(rpsLogic())
+    }
   }
 
+  const rpsLogic = (playerOneChoice) => {
+    if (playerOneChoice === 'rock') {
+      return (playerChoices[1] === 'scissors' ? 'player one wins' : 'player two wins')
+    } else if (playerOneChoice === 'paper') {
+      return (playerChoices[1] === 'rock' ? 'player one wins' : 'player two wins')
+    } else {
+      return (playerChoices[1] === 'paper' ? 'player one wins' : 'player two wins')
+    }
+  }
+
+  const checkIfGameOver = () => {
+    if (havePlayersChosen[0] === true && havePlayersChosen[1] === true) {
+      console.log('game over')
+    }
+  }
 
   const playerOneChooses = (e) => {
-    playerOneChoice = `one-${e.toElement.textContent}`
+    playerChoices[0] = e.toElement.textContent
     document.querySelector('.player-one .options').innerHTML = '<p> Player One has chosen </p>'
-    console.log(playerOneChoice)
+    havePlayersChosen[0] = true
+    // console.log(havePlayersChosen)
+    decision()
+    console.log(playerChoices)
   }
 
   const playerTwoChooses = (e) => {
-    playerTwoChoice = `two-${e.toElement.textContent}`
+    playerChoices[1] = e.toElement.textContent
     document.querySelector('.player-two .options').innerHTML = '<p> Player Two has chosen </p>'
-    console.log(playerTwoChoice)
+    havePlayersChosen[1] = true
+    // console.log(havePlayersChosen)
+    decision()
+    console.log(playerChoices)
   }
 
   [...playerOneButtons].map(button => {
