a1035c27bcf876e0004ba85e09cd9805a8111c58
Chris Terrazas <chris.terrazas@gmail.com>
2018-10-31 13:58:40 -0400

    I was wrong, now logic works

--- public/main.js
+++ public/main.js
@@ -9,23 +9,24 @@ const main = () => {
     if (playerChoices[0] === playerChoices[1]) {
       console.log('it\'s a tie')
     } else {
-      console.log(rpsLogic())
+      console.log(rpsLogic(playerChoices[0]))
     }
   }
 
   const rpsLogic = (playerOneChoice) => {
-    if (playerOneChoice === 'rock') {
-      return (playerChoices[1] === 'scissors' ? 'player one wins' : 'player two wins')
-    } else if (playerOneChoice === 'paper') {
-      return (playerChoices[1] === 'rock' ? 'player one wins' : 'player two wins')
+    let playerTwoChoice = playerChoices[1]
+    if (playerOneChoice === 'Rock') {
+      return playerTwoChoice === 'Scissors' ? 'player one wins' : 'player two wins'
+    } else if (playerOneChoice === 'Paper') {
+      return playerTwoChoice === 'Rock' ? 'player one wins' : 'player two wins'
     } else {
-      return (playerChoices[1] === 'paper' ? 'player one wins' : 'player two wins')
+      return playerTwoChoice === 'Paper' ? 'player one wins' : 'player two wins'
     }
   }
 
-  const checkIfGameOver = () => {
+  const checkIfGameStatus = () => {
     if (havePlayersChosen[0] === true && havePlayersChosen[1] === true) {
-      console.log('game over')
+      decision()
     }
   }
 
@@ -34,7 +35,7 @@ const main = () => {
     document.querySelector('.player-one .options').innerHTML = '<p> Player One has chosen </p>'
     havePlayersChosen[0] = true
     // console.log(havePlayersChosen)
-    decision()
+    checkIfGameStatus()
     console.log(playerChoices)
   }
 
@@ -43,11 +44,11 @@ const main = () => {
     document.querySelector('.player-two .options').innerHTML = '<p> Player Two has chosen </p>'
     havePlayersChosen[1] = true
     // console.log(havePlayersChosen)
-    decision()
+    checkIfGameStatus()
     console.log(playerChoices)
   }
 
-  [...playerOneButtons].map(button => {
+  const something = [...playerOneButtons].map(button => {
     button.addEventListener('click', playerOneChooses)
   })
 
