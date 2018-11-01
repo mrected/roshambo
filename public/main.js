a835be7886cc4646a76672d59a82a52ef81a67b4
Chris Terrazas <chris.terrazas@gmail.com>
2018-10-31 23:20:54 -0400

    done

--- public/main.js
+++ public/main.js
@@ -4,6 +4,15 @@ const main = () => {
   const playerTwoButtons = document.querySelectorAll('.player-two li')
   let playerChoices = []
   let havePlayersChosen = [false, false]
+  let winner = ''
+
+  let winnerCountDown = () => {
+    let winnerCircle = document.querySelector('.winner-circle')
+    winnerCircle.innerHTML = '<p class="fight-text">FIGHT</p>'
+    setTimeout(() => {
+      winnerCircle.innerHTML = `<p>Winner: <br><span>${winner}</span></p>`
+    }, 2000)
+  }
 
   const checkIfGameStatus = () => {
     if (havePlayersChosen[0] === true && havePlayersChosen[1] === true) {
@@ -13,33 +22,37 @@ const main = () => {
 
   const decision = () => {
     if (playerChoices[0] === playerChoices[1]) {
-      console.log('it\'s a tie')
+      winner = 'it\'s a tie'
+      winnerCountDown()
     } else {
-      console.log(rpsLogic(playerChoices[0]))
+      winner = rpsLogic(playerChoices[0])
+      winnerCountDown()
     }
   }
 
   const rpsLogic = (playerOneChoice) => {
     let playerTwoChoice = playerChoices[1]
     if (playerOneChoice === 'rock') {
-      return playerTwoChoice === 'scissors' ? 'player one wins' : 'player two wins'
+      return playerTwoChoice === 'scissors' ? 'player one' : 'player two'
     } else if (playerOneChoice === 'paper') {
-      return playerTwoChoice === 'rock' ? 'player one wins' : 'player two wins'
+      return playerTwoChoice === 'rock' ? 'player one' : 'player two'
     } else {
-      return playerTwoChoice === 'paper' ? 'player one wins' : 'player two wins'
+      return playerTwoChoice === 'paper' ? 'player one' : 'player two'
     }
   }
 
   const playerOneChooses = (e) => {
     console.log(playerChoices[0] = e.toElement.alt)
-    document.querySelector('.player-one .options').innerHTML = '<p> Player One has chosen </p>'
+    document.querySelector('.choose-one').innerHTML = '<p class="has-chosen-one">has chosen</p>'
+    document.querySelector('.player-one .options').innerHTML = ''
     havePlayersChosen[0] = true
     checkIfGameStatus()
   }
 
   const playerTwoChooses = (e) => {
     playerChoices[1] = e.toElement.alt
-    document.querySelector('.player-two .options').innerHTML = '<p> Player Two has chosen </p>'
+    document.querySelector('.choose-two').innerHTML = '<p class="has-chosen-two">has chosen</p>'
+    document.querySelector('.player-two .options').innerHTML = ''
     havePlayersChosen[1] = true
     checkIfGameStatus()
   }
@@ -51,6 +64,7 @@ const main = () => {
   const playerTwoButtonArray = [...playerTwoButtons].map(button => {
     button.addEventListener('click', playerTwoChooses)
   })
+
 }
 
 document.addEventListener('DOMContentLoaded', main)
